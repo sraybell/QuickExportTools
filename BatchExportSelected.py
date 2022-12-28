19d235f6fa4982335da1b44ba3023437f57d62c3
Steven Raybell <sraybell@gmail.com>
2022-12-28 15:56:08 -0800

    More code refactoring
    Minor refactoring to address a few style concerns. Changed format
    strings to f strings to make more concise and cleaner to work with.

--- BatchExportSelected.py
+++ BatchExportSelected.py
@@ -1,23 +1,26 @@
+import bpy  # pylint: disable=import-error, wrong-import-position
+import os  # pylint: disable=import-error, wrong-import-position, wrong-import-order
+
+
 bl_info = {
-    "name": "Batch Export",
-    "author": "Steven Raybell",
-    "description": "Provides batch export operators of selected objects for selected formats.",  # pylint: disable=line-too-long
-    "version": (0, 4, 2),
-    "blender": (3, 4, 0),
-    "location": "File > Export",
-    "category": "Import-Export"
+    'name': 'Batch Export',
+    'author': 'Steven Raybell',
+    'description': 'Provides batch export operators of selected objects for selected formats.',  # pylint: disable=line-too-long
+    'version': (0, 4, 3),
+    'blender': (3, 4, 0),
+    'location': 'File > Export',
+    'category': 'Import-Export'
 }
 
 
-import os # pylint: disable=import-error, wrong-import-position
-import bpy # pylint: disable=import-error, wrong-import-position
-
-
 PROPS = [
-    ('export_normals_chk', bpy.props.BoolProperty(name='Include Normals', default=False)),
+    ('export_normals_chk', bpy.props.BoolProperty(
+        name='Include Normals', default=False)),
     ('export_uvs_chk', bpy.props.BoolProperty(name='Include UVs', default=False)),
-    ('export_materials_chk', bpy.props.BoolProperty(name='Export Materials (.mtl)', default=False, description='Exports an associated .mtl')),
-    ('apply_modifiers_chk', bpy.props.BoolProperty(name='Apply Modifiers', default=True, description='Whether to apply the viewport modifiers on export'))
+    ('export_materials_chk', bpy.props.BoolProperty(name='Export Materials (.mtl)',
+     default=False, description='Exports an associated .mtl')),
+    ('apply_modifiers_chk', bpy.props.BoolProperty(name='Apply Modifiers',
+     default=True, description='Whether to apply the viewport modifiers on export'))
 ]
 
 
@@ -25,26 +28,24 @@ def batch_export_obj(self, context):
     blend_file = context.blend_data
 
     if blend_file.is_saved is False:
-        self.report({'ERROR'}, "The file has not been saved. " +
-                    "Unable to determine where to export OBJs!")
+        self.report({'ERROR'}, 'The file has not been saved. ' +
+                    'Unable to determine where to export OBJs!')
         return
 
     basedir = bpy.path.abspath('//objs/')
-    basedir_exists = os.path.exists(basedir)
-    if not basedir_exists:
-        os.makedirs(basedir)
+    os.makedirs(basedir, exist_ok=True)
 
     # store selection
     objs = context.selected_objects
 
     bpy.ops.object.select_all(action='DESELECT')
 
-    meshes = filter(lambda var: var.type == "MESH", objs)
+    meshes = filter(lambda var: var.type == 'MESH', objs)
     for mesh in meshes:
         mesh.select_set(True)
 
         bpy.ops.wm.obj_export(
-            filepath=os.path.join(basedir, '{}.obj'.format(mesh.name)),
+            filepath=os.path.join(basedir, f'{mesh.name}.obj'),
             export_selected_objects=True,
             export_materials=context.scene.export_materials_chk,
             export_uv=context.scene.export_uvs_chk,
@@ -56,13 +57,13 @@ def batch_export_obj(self, context):
         # Deselect the object and move on to another if any more are left
         mesh.select_set(False)
 
-    self.report({'INFO'}, "The operation has completed! Check in: " + basedir)
+    self.report({'INFO'}, f'The operation has completed! Check in: {basedir}')
 
 
 class ExportSelectedAsObjOperator(bpy.types.Operator):
-    """Exports all selected objects as individual OBJ files using the object's name"""
-    bl_idname = "batchexport.exportobjs"
-    bl_label = "Batch Export Selected Objects as OBJ"
+    '''Exports all selected objects as individual OBJ files using the object's name'''
+    bl_idname = 'batchexport.exportobjs'
+    bl_label = 'Batch Export Selected Objects as OBJ'
     bl_options = {'REGISTER'}
 
     def execute(self, context):
@@ -76,7 +77,7 @@ class BatchExportPanel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Item'
-    bl_context = ".objectmode"
+    bl_context = '.objectmode'
 
     @classmethod
     def poll(cls, context):
@@ -92,10 +93,10 @@ class BatchExportPanel(bpy.types.Panel):
         col.operator(
             ExportSelectedAsObjOperator.bl_idname,
             icon_value=674,
-            text="Selected as OBJ")
+            text='Selected as OBJ')
 
 
-def menu_func_export(self, context):
+def menu_batchexport_func(self):
     self.layout.operator(ExportSelectedAsObjOperator.bl_idname)
 
 
@@ -104,20 +105,18 @@ def register():
         setattr(bpy.types.Scene, prop_name, prop_value)
 
     bpy.utils.register_class(ExportSelectedAsObjOperator)
-    # bpy.utils.register_class(BatchExportPreferences)
     bpy.utils.register_class(BatchExportPanel)
-    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+    bpy.types.TOPBAR_MT_file_export.append(menu_batchexport_func)
 
 
 def unregister():
     for (prop_name, _) in PROPS:
         delattr(bpy.types.Scene, prop_name)
 
-    # bpy.utils.unregister_class(BatchExportPreferences)
-    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_batchexport_func)
     bpy.utils.unregister_class(ExportSelectedAsObjOperator)
     bpy.utils.unregister_class(BatchExportPanel)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     register()
