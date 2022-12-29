08041dcb72f769212e8d0a176909fa0e31c6dd9f
Steven Raybell <sraybell@gmail.com>
2022-12-29 09:07:41 -0800

    Code Refactoring and Feature Exposure
    While the stl exporter supports batching, it still seemed useful to
    expose STLs as an option for exporting for my workflows. A bit of code
    refactoring to reduce code redundancy, and some minor clean up here and
    there.
    
    I also changed the defaults so that normals are now selected for export
    by default as most folks will want to use this.
    
    Verionsed to v0.4.5 for release

--- BatchExportSelected.py
+++ BatchExportSelected.py
@@ -1,12 +1,12 @@
-import bpy  # pylint: disable=import-error, wrong-import-position
-import os  # pylint: disable=import-error, wrong-import-position, wrong-import-order
+import bpy  # pylint: disable=import-error
+import os
 
 
 bl_info = {
     'name': 'Batch Export',
     'author': 'Steven Raybell',
-    'description': 'Provides batch export operators of selected objects for selected formats.',  # pylint: disable=line-too-long
-    'version': (0, 4, 3),
+    'description': 'Provides batch export operatoions for OBJ and STL.',
+    'version': (0, 4, 5),
     'blender': (3, 4, 0),
     'location': 'File > Export',
     'category': 'Import-Export'
@@ -15,25 +15,55 @@ bl_info = {
 
 PROPS = [
     ('export_normals_chk', bpy.props.BoolProperty(
-        name='Include Normals', default=False)),
-    ('export_uvs_chk', bpy.props.BoolProperty(name='Include UVs', default=False)),
-    ('export_materials_chk', bpy.props.BoolProperty(name='Export Materials (.mtl)',
+        name='Include Normals (OBJ)', default=True,
+        description='Write normal vector information for the mesh')),
+    ('export_uvs_chk', bpy.props.BoolProperty(
+        name='Include UVs (OBJ)', default=False,
+        description='Writes UVs for the mesh')),
+    ('export_materials_chk', bpy.props.BoolProperty(name='Export Materials (OBJ)',
      default=False, description='Exports an associated .mtl')),
-    ('apply_modifiers_chk', bpy.props.BoolProperty(name='Apply Modifiers',
+    ('apply_modifiers_chk', bpy.props.BoolProperty(name='Apply Modifiers (OBJ, STL)',
      default=True, description='Whether to apply the viewport modifiers on export'))
 ]
 
 
-def batch_export_obj(self, context):
+def check_export(self, context, output_dir):
     blend_file = context.blend_data
-
     if blend_file.is_saved is False:
         self.report({'ERROR'}, 'The file has not been saved. ' +
-                    'Unable to determine where to export OBJs!')
+                    'Unable to determine where to export files!')
+        return False, ''
+    else:
+        basedir = bpy.path.abspath(f'//{output_dir}\\')
+        os.makedirs(basedir, exist_ok=True)
+
+        return True, basedir
+
+
+def batch_export_stl(self, context):
+    (result, basedir) = check_export(self, context, 'stls')
+    if result is False:
         return
 
-    basedir = bpy.path.abspath('//objs/')
-    os.makedirs(basedir, exist_ok=True)
+    bpy.ops.export_mesh.stl(
+        filepath=basedir,
+        check_existing=False,
+        use_selection=True,
+        use_mesh_modifiers=context.scene.apply_modifiers_chk,
+        batch_mode='OBJECT',
+        axis_forward='-Z',
+        axis_up='X'
+    )
+
+    bpy.ops.object.select_all(action='DESELECT')
+
+    self.report({'INFO'}, f'The operation has completed! Check in: {basedir}')
+
+
+def batch_export_obj(self, context):
+    (result, basedir) = check_export(self, context, 'objs')
+    if result is False:
+        return
 
     # store selection
     objs = context.selected_objects
@@ -71,6 +101,17 @@ class ExportSelectedAsObjOperator(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class ExportSelectedAsStlOperator(bpy.types.Operator):
+    '''Exports all selected objects as individual STL files using the object's name'''
+    bl_idname = 'batchexport.exportstls'
+    bl_label = 'Batch Export Selected Objects as STL'
+    bl_options = {'REGISTER'}
+
+    def execute(self, context):
+        batch_export_stl(self, context)
+        return {'FINISHED'}
+
+
 class BatchExportPanel(bpy.types.Panel):
     bl_label = 'Batch Export'
     bl_idname = 'BATCHEXPORT_PT_exports_1'
@@ -90,7 +131,14 @@ class BatchExportPanel(bpy.types.Panel):
             row = col.row()
             row.prop(context.scene, prop_name)
 
-        col.operator(
+        row = col.row()
+        row.operator(
+            ExportSelectedAsStlOperator.bl_idname,
+            icon_value=674,
+            text='Selected as STL')
+
+        row = col.row()
+        row.operator(
             ExportSelectedAsObjOperator.bl_idname,
             icon_value=674,
             text='Selected as OBJ')
@@ -105,6 +153,7 @@ def register():
         setattr(bpy.types.Scene, prop_name, prop_value)
 
     bpy.utils.register_class(ExportSelectedAsObjOperator)
+    bpy.utils.register_class(ExportSelectedAsStlOperator)
     bpy.utils.register_class(BatchExportPanel)
     bpy.types.TOPBAR_MT_file_export.append(menu_batchexport_func)
 
@@ -115,6 +164,7 @@ def unregister():
 
     bpy.types.TOPBAR_MT_file_export.remove(menu_batchexport_func)
     bpy.utils.unregister_class(ExportSelectedAsObjOperator)
+    bpy.utils.unregister_class(ExportSelectedAsStlOperator)
     bpy.utils.unregister_class(BatchExportPanel)
 
 
