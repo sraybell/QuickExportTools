2897c9949457f7f47c1e8df8db8338cf698ca455
Steven Raybell <sraybell@gmail.com>
2024-09-02 11:08:59 -0700

    Updated Version Number
    Forgot to update version number!
    
    Signed-off-by: Steven Raybell <sraybell@gmail.com>

--- QuickExportTools.py
+++ QuickExportTools.py
@@ -6,7 +6,7 @@ bl_info = {
     'name': 'Quick Export Tools',  # change name to match new feature(s)
     'author': 'Steven Raybell (popcornbag)',
     'description': 'Provides quick batched and merged export operations for OBJ and STL.',
-    'version': (0, 5, 4),
+    'version': (0, 5, 6),
     'blender': (4, 2, 1),
     'location': 'File > Export',
     'category': 'Import-Export'
